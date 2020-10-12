0c5134197264f80b0a90f2c7838c40b7f287518d
luckysuper41 <corneille261998@gmail.com>
2020-10-12 22:23:07 +0200

    Updates

---
+++ precache-manifest.67a1cfb71b1866e84c2f824ebc2f214d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78a2b9bd85bff4ea8134b0f2458fa8c",
+    "url": "/tictactrip/index.html"
+  },
+  {
+    "revision": "d6a844c300a5dc70a754",
+    "url": "/tictactrip/static/css/main.0c191f6d.chunk.css"
+  },
+  {
+    "revision": "4f22e4d22d551c82acfa",
+    "url": "/tictactrip/static/js/2.b48b07bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactrip/static/js/2.b48b07bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a844c300a5dc70a754",
+    "url": "/tictactrip/static/js/main.cdf4a195.chunk.js"
+  },
+  {
+    "revision": "7bb0650e8345e03e83c3",
+    "url": "/tictactrip/static/js/runtime-main.b71306a5.js"
+  },
+  {
+    "revision": "4104d38c41b8be0ebdda002ee8117250",
+    "url": "/tictactrip/static/media/info1.4104d38c.svg"
+  },
+  {
+    "revision": "def6912887ff7c4f81951e442f24013e",
+    "url": "/tictactrip/static/media/info2.def69128.svg"
+  },
+  {
+    "revision": "9a9836f41abbaa41f7bf2ce44fe0ff95",
+    "url": "/tictactrip/static/media/info3.9a9836f4.svg"
+  },
+  {
+    "revision": "2c62cd6e7fad9bcca50526a5b35d5f94",
+    "url": "/tictactrip/static/media/logo.2c62cd6e.svg"
+  }
+]);
